8dd78df7ec2e2dcf30a1ee3315b77380bdb0455d
kr-cherish <cherish.p@knackroot.com>
2025-04-28 18:31:14 +0530

    Web3 eth

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 import Account from "@/components/Account";
+import NewAccount from "@/components/NewAccount";
+import Web3Personal from "@/components/Web3Personal";
 import AbiEncodeDecode from "@/components/abiEncodeDecode";
 import ContractTest from "@/components/contractTest";
 import ReadOnlyProviderDemo from "@/components/etherProvider";
 import SendEther from "@/components/etherjs";
+import SignTrasactionHome from "@/components/signTrasaction";
 import ReadSmartContract from "@/components/smartContract";
 import WalletInfo from "@/components/walletInfo";
 export default function Home() {
@@ -17,9 +20,13 @@ export default function Home() {
 
       {/* <ReadOnlyProviderDemo/> */}
 
-      <AbiEncodeDecode/>
-      <Account/>
-      <ContractTest/>
+      {/* <AbiEncodeDecode/> */}
+      {/* <Account/> */}
+      {/* <ContractTest/> */}
+      <Web3Personal/>
+      {/* <NewAccount/> */}
+      <SignTrasactionHome/>
+
     </div>
   );
 }
